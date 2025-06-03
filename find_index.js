584aea227e3ad47709d4dd4850838a6ce192edda
VinayakRao028 <vinayak.rao@techolution.com>
2025-06-03 17:16:12 +0530

    Update find_index.js

--- find_index.js
+++ find_index.js
@@ -7,3 +7,20 @@ var allCustomers=["priya","alka","pawan"];
   
   findINdex(allCustomers);
 
+//Function to find a particular entry by its position/index (index in the array)
+var allCustomers=["priya","alka","pawan"];
+  function findINdex(customers){
+    var index=customers.indexOf("pawan"); // Assigning index to a variable.
+    console.log(index);
+  }
+  
+  findINdex(allCustomers);
+
+//Function to find a particular entry by its position/index (index in the array)
+var allCustomers=["priya","alka","pawan"];
+  function findINdex(customers){
+    var index=customers.indexOf("pawan"); // Assigning index to a variable.
+    console.log(index);
+  }
+  
+  findINdex(allCustomers);
